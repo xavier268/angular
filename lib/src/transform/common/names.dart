1e2de9c1555f48e7d5bc670569537a9b5f749234
Timothy Blasi <kegluneq@users.noreply.github.com>
2016-02-01 17:06:38 +0000

    7e5ec49 test(dart/transform): Update unit tests to expect code in <file>.template.dart

--- lib/src/transform/common/names.dart
+++ lib/src/transform/common/names.dart
@@ -6,10 +6,8 @@ const SETUP_METHOD_NAME = 'initReflector';
 const REFLECTOR_VAR_NAME = 'reflector';
 const TRANSFORM_DYNAMIC_MODE = 'transform_dynamic';
 const CSS_EXTENSION = '.css';
-const DEFERRED_EXTENSION = '.dart.deferredCount';
 const SHIMMED_STYLESHEET_EXTENSION = '.css.shim.dart';
 const NON_SHIMMED_STYLESHEET_EXTENSION = '.css.dart';
-const DEPS_EXTENSION = '.ng_deps.dart';
 const META_EXTENSION = '.ng_meta.json';
 const REFLECTION_CAPABILITIES_NAME = 'ReflectionCapabilities';
 const REFLECTOR_IMPORT = 'package:angular2/src/core/reflection/reflection.dart';
@@ -23,11 +21,8 @@ const TEMPLATE_EXTENSION = '.template.dart';
 
 /// Note that due to the implementation of `_toExtension`, ordering is
 /// important. For example, putting '.dart' first in this list will cause
-/// incorrect behavior because it will (incompletely) match '.template.dart'
-/// files.
+/// incorrect behavior.
 const ALL_EXTENSIONS = const [
-  DEFERRED_EXTENSION,
-  DEPS_EXTENSION,
   META_EXTENSION,
   SUMMARY_META_EXTENSION,
   TEMPLATE_EXTENSION,
@@ -41,8 +36,6 @@ const ALL_EXTENSIONS = const [
 /// any files named like transformer outputs will be reported as generated.
 bool isGenerated(String uri) {
   return const [
-    DEFERRED_EXTENSION,
-    DEPS_EXTENSION,
     META_EXTENSION,
     NON_SHIMMED_STYLESHEET_EXTENSION,
     SHIMMED_STYLESHEET_EXTENSION,
@@ -51,17 +44,9 @@ bool isGenerated(String uri) {
   ].any((ext) => uri.endsWith(ext));
 }
 
-/// Returns `uri` with its extension updated to [DEFERRED_EXTENSION].
-String toDeferredExtension(String uri) =>
-    _toExtension(uri, ALL_EXTENSIONS, DEFERRED_EXTENSION);
-
 /// Returns `uri` with its extension updated to [META_EXTENSION].
 String toMetaExtension(String uri) =>
     _toExtension(uri, ALL_EXTENSIONS, META_EXTENSION);
-
-/// Returns `uri` with its extension updated to [DEPS_EXTENSION].
-String toDepsExtension(String uri) =>
-    _toExtension(uri, ALL_EXTENSIONS, DEPS_EXTENSION);
 
 /// Returns `uri` with its extension updated to [TEMPLATES_EXTENSION].
 String toTemplateExtension(String uri) =>
